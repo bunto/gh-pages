97560bf3bd06f9058ec6aac017c7d9cb95c2c6b9
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-05-20 13:57:01 +0200

    Create gh-pages-dependencies_spec.rb

---
+++ spec/gh-pages-dependencies_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+describe(GitHubPages::Dependencies) do
+  CORE_DEPENDENCIES = %w(
+    bunto kramdown liquid rouge bunto-sass-converter
+    gh-pages-health-check listen
+  ).freeze
+  PLUGINS = described_class::VERSIONS.keys - CORE_DEPENDENCIES
+
+  it "exposes its gem versions" do
+    expect(described_class.gems).to be_a(Hash)
+  end
+
+  it "exposes relevant versions of dependencies, self and Ruby" do
+    expect(described_class.versions).to be_a(Hash)
+    expect(described_class.versions).to include("ruby")
+    expect(described_class.versions).to include("gh-pages")
+  end
+
+  context "bunto core dependencies" do
+    CORE_DEPENDENCIES.each do |gem|
+      it "exposes #{gem} dependency version" do
+        expect(described_class.gems[gem]).to be_a(String)
+        expect(described_class.gems[gem]).not_to be_empty
+      end
+    end
+  end
+
+  context "plugins" do
+    PLUGINS.each do |plugin|
+      it "whitelists the #{plugin} plugin" do
+        expect(GitHubPages::Configuration::PLUGIN_WHITELIST).to include(plugin)
+      end
+    end
+  end
+
+  it "exposes versions as Strings only" do
+    described_class.versions.values.each do |version|
+      expect(version).to be_a(String)
+    end
+  end
+end
